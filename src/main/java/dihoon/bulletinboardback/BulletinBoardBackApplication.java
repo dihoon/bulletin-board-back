02030855af9bc918962352e538e1436b760c2564
dihoon <dihoon955@gmail.com>
2024-07-15 10:27:34 +0900

    init

---
+++ src/main/java/dihoon/bulletinboardback/BulletinBoardBackApplication.java
@@ -0,0 +1,13 @@
+package dihoon.bulletinboardback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoardBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulletinBoardBackApplication.class, args);
+    }
+
+}
